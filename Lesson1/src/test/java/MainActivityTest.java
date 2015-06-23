230e93f1e0768a38863f2785f30b3c190a2a6bee
oudavid <ewhsoud@yahoo.com>
2015-06-23 15:49:53 -0700

    Apply AppCompat theme and activity. Verify Robolectric test with an assertion on text content.

--- Lesson1/src/test/java/MainActivityTest.java
+++ Lesson1/src/test/java/MainActivityTest.java
@@ -1,10 +1,15 @@
 import android.support.v7.appcompat.BuildConfig;
+import android.widget.TextView;
+
+import com.uw.android310.lesson1.MainActivity;
+import com.uw.android310.lesson1.R;
 
 import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
 /**
@@ -20,7 +25,10 @@ public class MainActivityTest {
     }
 
     @Test
-    public void testSomething() throws Exception {
+    public void testContentOnActivityLoad() throws Exception {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
 
+        TextView content = (TextView) activity.findViewById(R.id.content);
+        Assert.assertEquals(content.getText().toString(), activity.getString(R.string.hello_world));
     }
 }
