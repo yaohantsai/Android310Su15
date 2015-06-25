9f5fe53a2bfb9f727f447d25763a1afc52f1401c
dkou86 <ewhsoud@yahoo.com>
2015-06-25 12:12:18 -0700

    Process the search intent for the search query. Display it on screen and store it for later use.

--- Lesson2/src/main/java/com/uw/android310/lesson2/SearchableActivity.java
+++ Lesson2/src/main/java/com/uw/android310/lesson2/SearchableActivity.java
@@ -1,17 +1,41 @@
 package com.uw.android310.lesson2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class SearchableActivity extends ActionBarActivity {
+public class SearchableActivity extends AppCompatActivity {
+    private TextView mSearchQueryTextView;
+
+    private String mSearchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+
+        mSearchQueryTextView = (TextView) findViewById(R.id.searchQuery);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String unSanitizedQuery = intent.getStringExtra(SearchManager.QUERY);
+
+            // Sanitize the search query
+            String sanitizedQuery = sanitizeQueryString(unSanitizedQuery);
+
+            // Show the query on the device
+            mSearchQueryTextView.setText(sanitizedQuery);
+
+            // Store for later use
+            mSearchQuery = sanitizedQuery;
+        }
     }
 
     @Override
@@ -34,5 +58,16 @@ public class SearchableActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Sanitize a search query string by filtering inappropriate queries.
+     *
+     * @param unSanitizedQueryString
+     * @return sanitized query string that is safe to search for.
+     */
+    private String sanitizeQueryString(@NonNull String unSanitizedQueryString) {
+        // TODO: Implement String sanitize and filter curse words, inappropriate search results, etc.
+        return unSanitizedQueryString;
     }
 }
