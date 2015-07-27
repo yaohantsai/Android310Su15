d8eabd7a7b626ae40de4631664db93454ee02e78
dkou86 <ewhsoud@yahoo.com>
2015-07-27 12:41:50 -0700

    Checkpoint 6
    - onClick for imageView
    - ChooseFileIntent will call through onActivityResult
    - Check for FILE_PICK request code
    - Check for RESULT_OK
    - Use DocumentUtils.java to get the fully qualified path
    - Store the file in member field
    - Use Picasso to load file into the imageView

--- Lesson6/src/main/java/com/uw/android310/lesson6/activity/MainActivity.java
+++ Lesson6/src/main/java/com/uw/android310/lesson6/activity/MainActivity.java
@@ -1,18 +1,24 @@
 package com.uw.android310.lesson6.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 import com.uw.android310.lesson6.R;
 import com.uw.android310.lesson6.model.ImageUpload;
+import com.uw.android310.lesson6.util.DocumentUtils;
+import com.uw.android310.lesson6.util.IntentUtils;
 
 import java.io.File;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
 
+    /**
+     * Upload object containging image and meta data
+     */
+    private ImageUpload mUpload;
+
+    /**
+     * Chosen file from intent
+     */
+    private File mChosenFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +53,43 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
+    }
+
+    @OnClick(R.id.imageview)
+    public void onChooseImage() {
+        mUploadDesc.clearFocus();
+        mUploadTitle.clearFocus();
+        IntentUtils.chooseFileIntent(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode != IntentUtils.FILE_PICK) {
+            return;
+        }
+
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        Uri returnUri = data.getData();
+        String filePath = DocumentUtils.getPath(this, returnUri);
+
+        //Safety check to prevent null pointer exception
+        if (filePath == null || filePath.isEmpty()) {
+            return;
+        }
+
+        mChosenFile = new File(filePath);
+
+        Picasso.with(getBaseContext())
+                .load(mChosenFile)
+                .placeholder(R.drawable.ic_photo_library_black)
+                .fit()
+                .into(mUploadImage);
+
     }
 }
