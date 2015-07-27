7388331e2b3c82d1d95245c339e9061ffa0910ce
dkou86 <ewhsoud@yahoo.com>
2015-07-27 08:26:36 -0700

    Lesson 6 starter project

---
+++ Lesson5/src/main/java/com/uw/android310/lesson5/Weather.java
@@ -0,0 +1,16 @@
+package com.uw.android310.lesson5;
+
+/**
+ * Created by DOu on 7/20/15.
+ */
+public class Weather {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
